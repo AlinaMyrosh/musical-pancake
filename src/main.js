fa7437ee4edf1909112e030a3bb166426c64d03d
AlinaMyrosh <154742742+AlinaMyrosh@users.noreply.github.com>
2025-04-05 16:57:24 +0200

    update

--- src/main.js
+++ src/main.js
@@ -9,20 +9,39 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 20,
   keyboard: {
     enabled: true,
+    onlyInViewport: false,
   },
+  mousewheel: {
+    invert: true,
+  },
+
+  allowTouchMove: true,
+  touchEventsTarget: 'wrapper',
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  effect: 'cards',
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  on: {
+    init: function () {
+      console.log('swiper initialized');
+    },
+  },
+  // reachBeginning,
+  // reachEnd,
+  // disabledClass: 'projects-btn-disabled',
 });
 
-const sectionProjects = document.querySelector('.projects');
-sectionProjects.addEventListener('keydown', event => {
-  event.preventDefault();
-  if (event.code === 'ArrowRight') {
-    swiper.slideNext(300);
-  } else if (event.code === 'ArrowLeft') {
-    swiper.slidePrev(300);
-  }
-});
+// const sectionProjects = document.querySelector('.projects');
+// sectionProjects.addEventListener('keydown', event => {
+//   event.preventDefault();
+//   if (event.code === 'ArrowRight') {
+//     swiper.slideNext(300);
+//   } else if (event.code === 'ArrowLeft') {
+//     swiper.slidePrev(300);
+//   }
+// });
